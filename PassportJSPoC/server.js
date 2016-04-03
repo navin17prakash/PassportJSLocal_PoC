e9aeadb8364dcd2fd0c88abb1cfc693f530948ed
NAVIN PRAKASH <navin17prakash@gmail.com>
2016-04-04 02:20:55 +0530

    Working. Commit

--- PassportJSPoC/server.js
+++ PassportJSPoC/server.js
@@ -26,10 +26,31 @@ expressApp.set('view engine','ejs');
 expressApp.use(passport.initialize());
 expressApp.use(passport.session());
 
+passport.use('local',new passportLocal.Strategy(function (username , password, done) {
+    //Database call to verify the user name matches with the password 
+    //mocking the verification process now as if username ===  password then it is verified
+    
+    if(username === password){
+        done(null, {id : username , name : username});
+    }
+    else { done(null, null);}
+    
+}));
 
+passport.serializeUser(function (user, done ) {
+    done(null,user.id);
+    
+});
+
+passport.deserializeUser(function (id, done) {
+    //Query the DB or Cache
+    done(null, {id: id, name : id });
+    
+});
 expressApp.get('/',function homeroute(request, response) {
-    response.render('index',{name :'Gappy', isAuthenticated : false});
-    //response.send('Hello I am navin');
+    response.render('index',{name : request.user, 
+     isAuthenticated : request.isAuthenticated()});
+    
 });
 
 expressApp.get('/login',function loginroute(request, response) {
@@ -38,6 +59,13 @@ expressApp.get('/login',function loginroute(request, response) {
 });
 
 expressApp.post('/login', passport.authenticate('local'), function(request, response){
+    response.redirect('/'); // Redirrect to the login page 
+    
+});
+
+expressApp.get('/api/getdata',function(request, response){
+    //This function is yet to be written. It adds an authroisded route ie an API which provides some data to a user if he has successfully logged on      
+    
     
 });
 expressApp.listen(port);
